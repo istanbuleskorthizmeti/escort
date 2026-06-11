e891ff63319faefbc19c7f4a6ab2cca348abb0c5
Dorukcan Ay <info@dorukcanay.digital>
2026-06-11 05:02:27 +0300

    feat: add ad network verification file 2791365.txt

--- remote_transfer.js
+++ remote_transfer.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const { NodeSSH } = require('node-ssh');
 const ssh = new NodeSSH();
 
 async function run() {
   try {
     await ssh.connect({
-      host: '213.232.235.181',
-      username: 'root',
-      password: '4TVuj7qiHMfh7CxH6K!'
+      host: process.env.ATTACK_SERVER_IP || '187.77.111.203',
+      username: process.env.ATTACK_SERVER_USER || 'root',
+      password: process.env.ATTACK_SERVER_PASS || 'Z4-nN8JfiUIh5,;g'
     });
     
+    console.log(`Connected to ${process.env.ATTACK_SERVER_IP || '187.77.111.203'}`);
     const action = process.argv[2];
     if (action === 'download') {
       await ssh.getFile(process.argv[4], process.argv[3]);
